379ce1c57296fc87dd018c3a55e5c3662b18d41c
Eloy Espinaco <eloyesp@gmail.com>
2016-06-26 02:46:55 -0300

    Local storage

--- application.js
+++ application.js
@@ -1,18 +1,38 @@
+var store = require('localforage')
+
+var listaOración = document.getElementById("listaOracion"); // hace referencia a la lista, permite encontrarla en el html y le pone nombre
+
+function agregar_item (texto) {
+  var nuevoItem = document.createElement("li"); // crea el nuevo item que va ir a la lista
+
+  listaOracion.appendChild(nuevoItem); //agrega     nuevoItem a listaOración en ultimo lugar
+  nuevoItem.innerHTML = texto
+}
+
 function handleSubmit (evt) {// el evento es que se apriete el boton o sea se submitee el form
-  evt.preventDefault();
-  var campo = evt.target[0];
+  evt.preventDefault()
+  var campo = evt.target[0]
   if (campo.value !== "") { // si el campo está lleno
-
-    var nuevoItem = document.createElement("li"); // crea el nuevo item que va ir a la lista
-    var listaOración = document.getElementById("listaOracion"); // hace referencia a la lista, permite encontrarla en el html y le pone nombre
-
-    listaOracion.appendChild(nuevoItem); //agrega     nuevoItem a listaOración en ultimo lugar
-    nuevoItem.innerHTML = campo.value;
-
-    var today= new date ();
-    var year = today.getFullYear();
+    agregar_item(campo.value)
+    store.getItem('lista')
+      .then(function (lista) {
+        var lista = lista || []
+        lista.push(campo.value)
+        return store.setItem('lista', lista)
+      })
+      .then(function (value) {
+        console.log('stored:', value)
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
   }
 }
 
 //event listener
 document.addEventListener("submit", handleSubmit, false);
+
+store.getItem('lista')
+  .then(function (lista) {
+    lista.forEach(agregar_item);
+  })
